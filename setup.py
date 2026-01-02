51c060dfe0a8fa95d1a82ddf25c59a20539a41c3
Hossein Karami <49790581+hosseinkaramii@users.noreply.github.com>
2026-01-02 13:33:54 +0330

    docs: Pypi README updated

--- setup.py
+++ setup.py
@@ -7,16 +7,11 @@ setup(
     name="near-jsonrpc-client",
     version=PACKAGE_VERSION,
     description="A Python client for interacting with NEAR JSON-RPC API",
-    long_description = "This is a Python client for interacting with the NEAR Protocol using JSON-RPC.",
+    long_description="This is a Python client for interacting with the NEAR Protocol using JSON-RPC.",
     packages=find_packages(include=["near_jsonrpc_client*", "near_jsonrpc_models*"]),
     install_requires=[
         "httpx>=0.24",
         "pydantic>=2.0",
     ],
-    python_requires=">=3.9",
-    entry_points={
-        "console_scripts": [
-            "near-jsonrpc-client=cli:main",
-        ],
-    },
+    python_requires=">=3.9"
 )
