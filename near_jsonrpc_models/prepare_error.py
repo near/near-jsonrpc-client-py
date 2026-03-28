826b499a13e922c3b02ec6639c19cbf7cb5c3e7a
Hossein Karami <h.karami1811@gmail.com>
2026-03-28 03:56:12 +0330

    chore: regenerate client from OpenAPI (#51)
    Co-authored-by: GitHub Actions Bot <automation@github.com>

--- near_jsonrpc_models/prepare_error.py
+++ near_jsonrpc_models/prepare_error.py
@@ -5,12 +5,12 @@ This most likely indicates the module isn't valid.StackHeightInstrumentation: St
 This  most likely indicates the module isn't valid.Instantiate: Error happened during instantiation.
 
 This might indicate that `start` function trapped, or module isn't
-instantiable and/or un-linkable.Memory: Error creating memory.TooManyFunctions: Contract contains too many functions.TooManyLocals: Contract contains too many locals.TooManyTables: Contract contains too many tables.TooManyTableElements: Contract contains too many table elements."""
+instantiable and/or un-linkable.Memory: Error creating memory.TooManyFunctions: Contract contains too many functions.TooManyLocals: Contract contains too many locals.TooManyTables: Contract contains too many tables.TooManyTableElements: Contract contains too many table elements.FunctionBodyTooLarge: A function body in the contract exceeds the size limit.InstrumentedCodeTooLarge: The instrumented code exceeds the size limit."""
 
 from pydantic import RootModel
 from typing import Literal
 
 
-class PrepareError(RootModel[Literal['Serialization', 'Deserialization', 'InternalMemoryDeclared', 'GasInstrumentation', 'StackHeightInstrumentation', 'Instantiate', 'Memory', 'TooManyFunctions', 'TooManyLocals', 'TooManyTables', 'TooManyTableElements']]):
+class PrepareError(RootModel[Literal['Serialization', 'Deserialization', 'InternalMemoryDeclared', 'GasInstrumentation', 'StackHeightInstrumentation', 'Instantiate', 'Memory', 'TooManyFunctions', 'TooManyLocals', 'TooManyTables', 'TooManyTableElements', 'FunctionBodyTooLarge', 'InstrumentedCodeTooLarge']]):
     pass
 
